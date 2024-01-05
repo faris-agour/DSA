b5182be6c4bc535186823241cd1b169ee5a535f1
fares_agour <faresabouagour@gmail.com>
2024-01-05 15:04:47 +0200

    edit in stack

--- stack.py
+++ stack.py
@@ -5,10 +5,9 @@ class Node:
 
 
 class Stack:
-    def __init__(self, value):
-        node = Node(value)
-        self.top = node
-        self.length = 1
+    def __init__(self):
+        self.top = None
+        self.length = 0
 
     def push(self, value):
         node = Node(value)
@@ -20,20 +19,17 @@ class Stack:
         self.length += 1
         return True
 
+    def is_not_empty(self):
+        return True if self.length > 0 else False
+
     def pop(self):
-        temp = self.top
         if self.length == 0:
             return None
 
-        if self.length == 1:
-            self.top = None
-            self.length -= 1
-            return temp
-
+        temp = self.top
         self.top = temp.next
-        temp.next = None
         self.length -= 1
-        return temp
+        return temp.value
 
     def print_stack(self):
         temp = self.top
@@ -41,16 +37,16 @@ class Stack:
             print(temp.value)
             temp = temp.next
 
-
-stack = Stack(2)
-
-stack.push(4)
-stack.push(6)
-
-stack.pop()
-
-# stack.pop()  
-stack.print_stack()
-print('----------')
-
-print(stack.top.value)
+#
+# stack = Stack(2)
+#
+# stack.push(4)
+# stack.push(6)
+#
+# stack.pop()
+#
+# # stack.pop()
+# stack.print_stack()
+# print('----------')
+#
+# print(stack.top.value)
